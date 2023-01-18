f7cac9f769bd6abf85b977dd850fe82c197ff2bc
DΛn9Tuaz <85827570+bachdangtuan@users.noreply.github.com>
2023-01-19 02:47:32 +0700

    cập nhật side bar

--- src/app/apps/apps-routing.module.ts
+++ src/app/apps/apps-routing.module.ts
@@ -4,7 +4,7 @@ import {RouterModule, Routes} from '@angular/router';
 const routes: Routes = [
     {path: 'company', loadChildren: () => import('./company/company.module').then(m => m.CompanyModule)},
     {path: 'trip', loadChildren: () => import('./trip/trip.module').then(m => m.TripModule)},
-
+    {path: 'vehicle', loadChildren: () => import('./vehicle/vehicle.module').then(m => m.VehicleModule)},
 
     // {path: 'calendar', loadChildren: () => import('./calendar/calendar.module').then(m => m.CalendarModule)},
     // {path: 'chat', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule)},
