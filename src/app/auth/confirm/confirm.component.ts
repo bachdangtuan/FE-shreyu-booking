0440d90e92a678821a65e70668d878f28bf2fc22
DΛn9Tuaz <85827570+bachdangtuan@users.noreply.github.com>
2023-01-19 01:53:29 +0700

    xử lý xong phần tích hợp đăng nhập và đăng ký (#2)

--- src/app/auth/confirm/confirm.component.ts
+++ src/app/auth/confirm/confirm.component.ts
@@ -1,17 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import {Component, OnInit} from '@angular/core';
+import {Title} from '@angular/platform-browser';
+import {TITLE} from "../../core/constants/common";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-confirm',
-  templateUrl: './confirm.component.html',
-  styleUrls: ['./confirm.component.scss']
+    selector: 'app-confirm',
+    templateUrl: './confirm.component.html',
+    styleUrls: ['./confirm.component.scss']
 })
 export class ConfirmComponent implements OnInit {
+    count = 5;
 
-  constructor (private titleService: Title) { }
+    constructor(private titleService: Title,
+                private router: Router) {
+    }
 
-  ngOnInit(): void {
-    this.titleService.setTitle("Confirm Mail | Shreyu - Responsive Angular and Bootstrap 5 Admin Dashboard Template")
-  }
+    ngOnInit(): void {
+        this.titleService.setTitle(TITLE.REGISTER)
+        this.countDownNumber()
+    }
+
+
+    countDownNumber() {
+        const intervalId = setInterval(() => {
+            this.count--;
+            console.log('cout', this.count)
+            if (this.count == 0) {
+                clearInterval(intervalId)
+                this.router.navigate(['/auth/login']).then();
+            }
+        }, 800)
+    }
 
 }
